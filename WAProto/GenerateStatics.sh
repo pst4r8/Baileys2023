dec2a7db563444000089ba432fc65e0c490aeff1
Pst4r8 <94172938+pst4r8@users.noreply.github.com>
2024-12-24 20:34:50 +0700

    Update GenerateStatics.sh

--- WAProto/GenerateStatics.sh
+++ WAProto/GenerateStatics.sh
@@ -1,4 +1,13 @@
-yarn pbjs -t static-module -w commonjs -o ./WAProto/index.js ./WAProto/WAProto.proto;
-yarn pbts -o ./WAProto/index.d.ts ./WAProto/index.js;
+#yarn pbjs -t static-module -w commonjs -o ./WAProto/index.js ./WAProto/WAProto.proto;
+#yarn pbts -o ./WAProto/index.d.ts ./WAProto/index.js;
 
 #protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=env=node,useOptionals=true,forceLong=long --ts_proto_out=. ./src/Binary/WAMessage.proto;
+
+#!/bin/sh
+
+# Ensure the commands are available
+PBJS=$(yarn bin)/pbjs
+PBTS=$(yarn bin)/pbts
+
+$PBJS -t static-module -w commonjs -o WAProto.js WAProto.proto
+$PBTS -o WAProto.d.ts WAProto.js
